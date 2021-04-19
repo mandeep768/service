0bb2d45f1d8620fc19e37f36b87cd986adeb3125
mandeep768 <smandeepsingh768@gmail.com>
2021-04-19 07:30:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+  <section id="our-services">
+  <div className="container">
+    <div className="desc">
+      <div className="heading">
+        <h1>Our Services</h1>
+      </div>
+      <div className="text">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, velit a bibendum vulputate, metus enim pretium massa, eu placerat lacus leo vitae velit. Donec rutrum est sed lacus scelerisque scelerisque a ac magna. Suspendisse. </p>
+      </div>
     </div>
+    <div className="services">
+
+      <div className="column" id="column-1">
+        <div className="service">
+          <div className="service-desc">
+            <i className="service-icon fas fa-flask">
+
+            </i>
+            <p className="service-txt">
+              bussiniss intelegince  
+            </p>                       
+          </div>
+        </div>
+        <div className="service">
+          <div className="service-desc">
+            <i className="service-icon fas fa-bullhorn">
+
+            </i>
+            <p className="service-txt">
+              digital marketing
+            </p>
+          </div>
+        </div>
+        <div className="service">
+          <div className="service-desc">
+            <i className="service-icon far fa-lightbulb">
+
+            </i>
+            <p className="service-txt">
+              PRODUCT DEVELOPMENT
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="column" id="column-2">
+        <div className="service">
+          <div className="service-desc">
+            <i className="service-icon fas fa-wrench">
+
+            </i>
+            <p className="service-txt">
+              CRM CUSTOMAIZATION
+            </p>
+          </div>
+        </div>
+        <div className="service">
+          <div className="service-desc">
+            <i className="service-icon fas fa-database">
+
+            </i>
+            <p className="service-txt">
+              BUSINESS INTELLIGENCE
+            </p>
+          </div>
+        </div>
+        <div className="service">
+          <div className="service-desc">
+            <i className="service-icon fas fa-chart-pie">
+
+            </i>
+            <p className="service-txt">
+              DATA AQUSITION
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="column" id="column-2">
+        <div className="service">
+          <div className="service-desc">
+            <i className="service-icon fas fa-tasks">
+
+            </i>
+            <p className="service-txt">
+              PROJECT MAGNEMENT
+            </p>                            
+          </div>
+        </div>
+        <div className="service">
+          <div className="service-desc">
+            <i className="service-icon fas fa-certificate">
+
+            </i>
+            <p className="service-txt">
+              SEO/SEM
+            </p>
+          </div>
+        </div>
+        <div className="service hidden">
+          <div className="service-desc">
+            <i className="service-icon">
+
+            </i>
+            <p className="service-txt">
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="clear-fix"></div>
+  </div>
+</section>
+    
+    </>
   );
 }
 
